717a234ff4aad94f7036519b38df92644f40942b
Robert <mccannb401@gmail.com>
2021-04-25 16:45:31 +0100

    Initial author routes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 //sets view engine
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -26,5 +27,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || 3001)
+
+app.listen(process.env.PORT || 3000)
